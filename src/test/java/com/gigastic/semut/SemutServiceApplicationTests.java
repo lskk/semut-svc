017979eaf8fc989b0832b22f7bdd16a65b589417
Hendy Irawan <ceefour666@gmail.com>
2016-12-16 19:32:53 +0700

    Initial commit

---
+++ src/test/java/com/gigastic/semut/SemutServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gigastic.semut;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SemutServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
